5f01cc1661cba6f6ff912a38c4e7ac9b19167ed2
FonDitbul <niki7084@naver.com>
2022-01-11 14:33:10 +0900

    Channel Controller, service 나머지 api 작성

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ChannelsService } from './channels.service';
 import { User } from '../common/decorators/user.decorator';
 import { UsersEntity } from '../entities/Users.entity';
+import { CreateChannelDto } from './dto/create-channel.dto';
 
 @ApiTags('CHANNLE')
 @Controller('channels')
@@ -19,5 +28,64 @@ export class ChannelsController {
   @Get(':url/channels/:name')
   async getWorkspaceChannel(@Param('url') url, @Param('name') name) {
     return this.channelsService.getWorkspaceChannel(url, name);
+  }
+
+  @ApiOperation({ summary: '워크스페이스 채널 만들기' })
+  @Post(':url/channels')
+  async createWorkspaceChannels(
+    @Param('url') url,
+    @Body() body: CreateChannelDto,
+    @User() user: UsersEntity,
+  ) {
+    return this.channelsService.createWorkspaceChannels(
+      url,
+      body.name,
+      user.id,
+    );
+  }
+
+  @ApiOperation({ summary: '워크스페이스 채널 멤버 가져오기' })
+  @Get(':url/channels/:name/members')
+  async getWorkspaceChannelMembers(
+    @Param('url') url: string,
+    @Param('name') name: string,
+  ) {
+    return this.channelsService.getWorkspaceChannelMembers(url, name);
+  }
+
+  @ApiOperation({ summary: '워크스페이스 채널 멤버 초대하기' })
+  @Post(':url/channels/:name/members')
+  async createWorkspaceMembers(
+    @Param('url') url: string,
+    @Param('name') name: string,
+    @Body('email') email,
+  ) {
+    return this.channelsService.createWorkspaceChannelMembers(url, name, email);
+  }
+
+  @ApiOperation({ summary: '워크스페이스 특정 채널 채팅 모두 가져오기' })
+  @Get(':url/channels/:name/chats')
+  async getWorkspaceChannelChats(
+    @Param('url') url,
+    @Param('name') name,
+    @Query('perPage', ParseIntPipe) perPage: number,
+    @Query('page', ParseIntPipe) page: number,
+  ) {
+    return this.channelsService.getWorkspaceChannelChats(
+      url,
+      name,
+      perPage,
+      page,
+    );
+  }
+
+  @ApiOperation({ summary: '안 읽은 개수 가져오기' })
+  @Get(':url/channels/:name/unreads')
+  async getUnreads(
+    @Param('url') url,
+    @Param('name') name,
+    @Query('after', ParseIntPipe) after: number,
+  ) {
+    return this.channelsService.getChannelUnreadsCount(url, name, after);
   }
 }
--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
 import { ChannelsEntity } from '../entities/Channels.entity';
 import { ChannelMembersEntity } from '../entities/ChannelMembers.entity';
 import { WorkspacesEntity } from '../entities/Workspaces.entity';
@@ -50,6 +50,92 @@ export class ChannelsService {
         name,
       },
       relations: ['Workspace'],
+    });
+  }
+
+  async createWorkspaceChannels(url: string, name: string, myId: number) {
+    const workspace = await this.workspacesRepository.findOne({
+      where: { url },
+    });
+    const channel = new ChannelsEntity();
+    channel.name = name;
+    channel.WorkspaceId = workspace.id;
+    const channelReturned = await this.channelsRepository.save(channel);
+    const channelMember = new ChannelMembersEntity();
+    channelMember.UserId = myId;
+    channelMember.ChannelId = channelReturned.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceChannelMembers(url: string, name: string) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .innerJoin('user.Channels', 'channels', 'channels.name = :name', {
+        name,
+      })
+      .innerJoin('channels.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getMany();
+  }
+
+  async createWorkspaceChannelMembers(url, name, email) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder('channel')
+      .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .where('channel.name = :name', { name })
+      .getOne();
+    if (!channel) throw new NotFoundException('채널이 존재하지 않습니다.');
+    const user = await this.usersRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .innerJoin('user.Workspaces', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getOne();
+    if (!user) throw new NotFoundException('사용자가 존재하지 않습니다');
+    const channelMember = new ChannelMembersEntity();
+    channelMember.ChannelId = channel.id;
+    channelMember.UserId = user.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceChannelChats(
+    url: string,
+    name: string,
+    perPage: number,
+    page: number,
+  ) {
+    return this.channelChatsRepository
+      .createQueryBuilder('channelChats')
+      .innerJoin('channelChats.Channel', 'channel', 'channel.name=:name', {
+        name,
+      })
+      .innerJoin('channel.Workpsace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .innerJoinAndSelect('channelChats.user', 'user')
+      .orderBy('channelChats.createdAt', 'DESC')
+      .take(perPage)
+      .skip(perPage * (page - 1))
+      .getMany();
+  }
+
+  async getChannelUnreadsCount(url, name, after) {
+    const channel = await this.channelsRepository
+      .createQueryBuilder('channel')
+      .innerJoin('channel.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .where('channel.name = :name', { name })
+      .getOne();
+    return this.channelChatsRepository.count({
+      where: {
+        ChannelId: channel.id,
+        createdAt: MoreThan(new Date(after)),
+      },
     });
   }
 }
