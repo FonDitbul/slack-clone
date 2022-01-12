ccaf38697a0df24da7400bd1b96b633c351d63de
FonDitbul <niki7084@naver.com>
2022-01-12 17:09:40 +0900

    e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import * as passport from 'passport';
+import * as session from 'express-session';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,6 +14,18 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.use(
+      session({
+        resave: false,
+        saveUninitialized: false,
+        secret: 'fdgdstwqetwqt',
+        cookie: {
+          httpOnly: true,
+        },
+      }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
     await app.init();
   });
 
@@ -20,5 +34,17 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  // superagent -> supertest
+  // axios -> moxios
+  it('/users/login (POST)', (done) => {
+    return request(app.getHttpServer())
+      .post('/api/users/login')
+      .send({
+        email: 'zerohch0@gmail.com',
+        password: 'nodejsbook',
+      })
+      .expect(201, done);
   });
 });
